7146768669d6fa00656c6484f981503085c3fb19
kiru04 <kirubelbelayneh@cmail.carleton.ca>
2023-11-08 12:34:53 -0500

    add todo item

--- script.js
+++ script.js
@@ -45,11 +45,12 @@
 
 // Initialise an empty array with the variable name todoItems
   let todoItems = [];
+  let count = 1 ;
 // Function to add a todo to the list
 // It should accept a string as a parameter (text of the todo item)
 // and it should add a new todo item to the todoItems array
 // the function does not need to return anything
-function addToDoItem(text) {
+function addTodoItem(text) {
   const todoItem = { text: text };
   todoItems.push(todoItem);
 }
